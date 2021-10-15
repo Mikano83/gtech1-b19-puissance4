b45d02c63ca6015b4af34001993e52cdc833b625
BuildTools <unconfigured@null.spigotmc.org>
2021-10-15 23:15:27 +0200

    Beta squelette

--- puissance4.c
+++ puissance4.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #define NBL 6
 #define NBC 7
-char plateau[NBL+3][NBC];
+char plateau[NBL][NBC];
+char joueur[2];
+char token[] = {'x','o'};
+int lineChanged;
 
 void init(){
     for (int L=0; L<NBL; L++){
@@ -12,9 +17,116 @@ void init(){
     }
 }
 
+void print_plateau(){
+    for (int C=0; C<NBC; C++){
+        printf("--");
+    }
+    printf("-\n ");
+    for (int L=0; L<NBL; L++){
+        if (L!=0)printf("\n ");
+        for (int C=0; C<NBC; C++){
+            char case_tableau = plateau[L][C];
+            printf("%c ", case_tableau);
+        }
+    }
+    printf("\n");
+    for (int C=0; C<NBC; C++){
+        printf("--");
+    }
+    printf("-\n ");
+    for (int C=1; C<=NBC; C++){
+        printf("%d ", C);
+    }
+    printf("\n\n");
+}
+
+void update_plateau(int colonne, int player){
+    for (int l=NBL; l>=0; l--){
+        if (plateau[l][colonne] == '.'){
+            plateau[l][colonne] = token[player];
+            lineChanged = l;
+            break;
+        }
+    }
+    print_plateau();
+}
+
+bool isWin(int player, int colonne){
+    int gauche = 1;
+    int droite = 1;
+    int bas = 1;
+    int dhGauche = 1;
+    int dhDroite = 1;
+    int dbGauche = 1;
+    int dbDroite = 1;
+
+    char pion = plateau[lineChanged][colonne];
+
+
+    for (int verif=1; verif<=4; verif++){                  /*Verification sur la gauche*/
+        if (plateau[lineChanged][colonne-verif] == pion){
+            gauche +=1;
+        }else break;
+    }
+    for (int verif=1; verif<=4; verif++){                  /*Verification sur la diagonale haut-gauche*/
+        if (plateau[lineChanged-verif][colonne-verif] == pion){
+            dhGauche +=1;
+        }else break;
+    }
+    for (int verif=1; verif<=4; verif++){                  /*Verification sur la droite*/
+        if (plateau[lineChanged][colonne+verif] == pion){
+            droite +=1;
+        }else break;
+    }
+    for (int verif=1; verif<=4; verif++){                  /*Verification sur la diagonale haut-droit*/
+        if (plateau[lineChanged-verif][colonne+verif] == pion){
+            dhDroite +=1;
+        }else break;
+    }
+    for (int verif=1; verif<=4; verif++){                  /*Verification sur le bas*/
+        if (plateau[lineChanged+verif][colonne] == pion){
+            bas +=1;
+        }else break;
+    }
+    for (int verif=1; verif<=4; verif++){                  /*Verification sur la diagonale bas-gauche*/
+        if (plateau[lineChanged+verif][colonne-verif] == pion){
+            dbGauche +=1;
+        }else break;
+    }
+    for (int verif=1; verif<=4; verif++){                  /*Verification sur la diagonale bas-droit*/
+        if (plateau[lineChanged+verif][colonne+verif] == pion){
+            dbDroite +=1;
+        }else break;
+    }
+    printf("%c%d%d%d%d%d%d%d\n\n", pion, gauche, dhGauche, droite, dhDroite, bas, dbGauche, dbDroite);
+    if (gauche >=4 || dhGauche >=4 || droite >=4 || dhDroite >=4 || bas >=4 || dbGauche >=4 || dbDroite >=4){
+        return true;
+    }else return false;
+    
+}
+
+void game(){
+    bool end = false;
+    int player = 0;
+    print_plateau();
+    do{
+        int colonne;
+        printf("Joueur %d: ", player+1);
+        scanf( "%d", &colonne );
+        colonne -= 1;
+        update_plateau(colonne, player);
+        
+        end = isWin(player, colonne);
+        player = !player;
+    } while (!end);
+    printf("Felicitation joueur %d !\n\nAppuyez sur entree pour quitter.", !player);
+    char bye = 'q';
+    scanf("%s", &bye);
+}
 
 int main(void){
     init();
+    game();
     
     return 0;
 }
