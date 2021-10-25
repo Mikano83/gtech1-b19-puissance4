75e23850b02985131bec5feb9de1398bfe2f90e0
unknown <tdasilva@gamingcampus.local>
2021-10-25 16:56:07 +0200

    Fin de cours && résolution de bugs liés à la victoire.

--- puissance4.c
+++ puissance4.c
@@ -12,13 +12,13 @@ int lineChanged;
 
 void init()
 {
-    for (int L = 0; L < NBL; L++)
+    /*for (int L = 0; L < NBL; L++)
     {
         for (int C = 0; C < NBC; C++)
         {
             plateau[L][C] = '.';
         }
-    }
+    }*/
 }
 
 void print_plateau()
@@ -80,7 +80,7 @@ int update_plateau(int colonne, int player)
     }
 }
 
-bool isWin(int player, int colonne)
+bool isWin(int player, int colonne, char pion)
 {
     int gauche = 1;
     int droite = 1;
@@ -90,44 +90,68 @@ bool isWin(int player, int colonne)
     int dbGauche = 1;
     int dbDroite = 1;
 
-    char pion = plateau[lineChanged][colonne];
-
     for (int verif = 1; verif <= 4; verif++)
     { /*Verification sur la gauche*/
         if (plateau[lineChanged][colonne - verif] == pion)
         {
             gauche += 1;
         }
-        /*Verification sur la diagonale haut-gauche*/
-        else if (plateau[lineChanged - verif][colonne - verif] == pion)
+        else
+            break;
+    }
+    for (int verif = 1; verif <= 4; verif++)
+    { /*Verification sur la diagonale haut-gauche*/
+        if (plateau[lineChanged - verif][colonne - verif] == pion)
         {
             dhGauche += 1;
         }
-        /*Verification sur la droite*/
-        else if (plateau[lineChanged][colonne + verif] == pion)
+        else
+            break;
+    }
+    for (int verif = 1; verif <= 4; verif++)
+    { /*Verification sur la droite*/
+        if (plateau[lineChanged][colonne + verif] == pion)
         {
             droite += 1;
         }
-        /*Verification sur la diagonale haut-droit*/
-        else if (plateau[lineChanged - verif][colonne + verif] == pion)
+        else
+            break;
+    }
+    for (int verif = 1; verif <= 4; verif++)
+    { /*Verification sur la diagonale haut-droit*/
+        if (plateau[lineChanged - verif][colonne + verif] == pion)
         {
             dhDroite += 1;
         }
-        /*Verification sur le bas*/
-        else if (plateau[lineChanged + verif][colonne] == pion)
+        else
+            break;
+    }
+    for (int verif = 1; verif <= 4; verif++)
+    { /*Verification sur le bas*/
+        if (plateau[lineChanged + verif][colonne] == pion)
         {
             bas += 1;
         }
-        /*Verification sur la diagonale bas-gauche*/
-        else if (plateau[lineChanged + verif][colonne - verif] == pion)
+        else
+            break;
+    }
+    for (int verif = 1; verif <= 4; verif++)
+    { /*Verification sur la diagonale bas-gauche*/
+        if (plateau[lineChanged + verif][colonne - verif] == pion)
         {
             dbGauche += 1;
         }
-        /*Verification sur la diagonale bas-droit*/
-        else if (plateau[lineChanged + verif][colonne + verif] == pion)
+        else
+            break;
+    }
+    for (int verif = 1; verif <= 4; verif++)
+    { /*Verification sur la diagonale bas-droit*/
+        if (plateau[lineChanged + verif][colonne + verif] == pion)
         {
             dbDroite += 1;
         }
+        else
+            break;
     }
     if (gauche + droite - 1 >= 4 || dhGauche + dbDroite - 1 >= 4 || dhDroite + dbGauche - 1 >= 4 || bas >= 4)
     {
@@ -186,7 +210,7 @@ void game(void)
                 switch (isError)
                 {
                 case 0:
-                    end = isWin(player, colonne);
+                    end = isWin(player, colonne, token[player]);
                     player = !player;
                     break;
 
